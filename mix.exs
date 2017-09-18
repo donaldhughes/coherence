683bd16664d0978d10b6abbebc681066e513094b
Donald Hughes <donald.hughes@gmail.com>
2017-09-18 10:51:36 -0500

    changes to allow coherence deps to work with Phoenix 1.3

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Coherence.Mixfile do
 
   defp deps do
     [
-      {:ecto, "~> 2.0"},
+      {:ecto, ">= 2.2.0"},
       {:comeonin, "~> 3.0"},
       {:phoenix, "~> 1.3"},
       {:phoenix_html, "~> 2.10"},
@@ -42,7 +42,7 @@ defmodule Coherence.Mixfile do
       {:uuid, "~> 1.0"},
       {:phoenix_swoosh, "~> 0.2"},
       {:timex, "~> 3.1"},
-      {:timex_ecto, "~> 3.1"},
+      {:timex_ecto, github: "bitwalker/timex_ecto", tag: "3.2.0"},
       {:floki, "~> 0.8", only: :test},
       {:ex_doc, "~> 0.16", only: :dev},
       {:earmark, "~> 1.2", only: :dev, override: true},
